7a178cd0bf9d88ff32e3fca6e313aae4b789b3af
Priyanka <97823620+priy-38@users.noreply.github.com>
2022-05-06 20:16:10 +0530

    Create Mini-Max Sum.cpp

---
+++ Assignment 1/Mini-Max Sum.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+void miniMaxSum(vector<int> arr) {
+    long max=0,min=0;
+    sort(arr.begin(),arr.end());
+    for (int i=0;i<4;i++)
+    {
+        min+=arr[i];
+    }
+    for (int j=1;j<5;j++)
+    {
+        max+=arr[j];
+    }
+    
+    cout<<min<<" "<<max;
+}
+
+int main()
+{
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<int> arr(5);
+
+    for (int i = 0; i < 5; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    miniMaxSum(arr);
