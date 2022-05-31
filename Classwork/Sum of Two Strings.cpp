7e6facb51d5eff7f723ec01c089f37c4111dc500
Priyanka <97823620+Priy-nka@users.noreply.github.com>
2022-05-31 09:28:54 +0530

    Update Sum of Two Strings.cpp

--- Classwork/Sum of Two Strings.cpp
+++ Classwork/Sum of Two Strings.cpp
@@ -1,64 +1,50 @@
 
 //https://leetcode.com/playground/ExpLp8Nt
 
+#include<bits/stdc++.h>
+
 string add(string a,string b)
 {
+    int dif;
     if (b.size()>a.size())
     {
+        dif=b.size()-a.size();
         reverse(a.begin(),a.end());
-        for (int i=0;i<(b.size()-a.size());i++)
+        for (int i=0;i<dif;i++)
         {
             a+="0";
         }
         reverse(a.begin(),a.end());
-        int len=b.size()-1;
-        string num="";
-        int carry=0;
-        for (int k=len;k>=0;k--)
-        {
-            if (k==0 && carry!=0)
-            {
-                string num2=to_string(((a[k]-'0')+(b[k]-'0')+carry));
-                reverse(num2.begin(),num2.end());
-                num+=num2;
-            }
-            else
-            {
-            num+=to_string(((a[k]-'0')+(b[k]-'0')+carry)%10);
-            carry=((a[k]-'0')+(b[k]-'0'))/10;
-            }
-        }
-        reverse(num.begin(),num.end());
-        return num;
     }
-    else
+    else if (b.size()<a.size())
     {
+        dif=a.size()-b.size();
         reverse(b.begin(),b.end());
-        for (int i=0;i<(a.size()-b.size());i++)
+        for (int i=0;i<dif;i++)
         {
             b+="0";
         }
         reverse(b.begin(),b.end());
-        int len=a.size()-1;
-        string num="";
-        int carry=0;
-        for (int k=len;k>=0;k--)
-        {
-            if (k==0 && carry!=0)
-            {
-                string num1=to_string(((b[k]-'0')+(a[k]-'0')+carry));
-                reverse(num1.begin(),num1.end());
-                num+=num1;
-            }
-            else
-            {
-            num+=to_string(((b[k]-'0')+(a[k]-'0')+carry)%10);
-            carry=((b[k]-'0')+(a[k]-'0'))/10;
-            }
-        }
-        reverse(num.begin(),num.end());
-        return num;
     }
+    int len=b.size()-1;
+    string num="";
+    int carry=0;
+    for (int k=len;k>=0;k--)
+    {
+        if (k==0 && carry!=0)
+        {
+            string num2=to_string(((a[k]-'0')+(b[k]-'0')+carry));
+            reverse(num2.begin(),num2.end());
+            num+=num2;
+        }
+        else
+        {
+        num+=to_string(((a[k]-'0')+(b[k]-'0')+carry)%10);
+        carry=((a[k]-'0')+(b[k]-'0'))/10;
+        }
+    }
+    reverse(num.begin(),num.end());
+    return num;
 }
 int main() {
     if(add("123","234")!= "357"){
